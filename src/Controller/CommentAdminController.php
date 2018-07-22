6576b85c7609a2c74f2d48c37291a35a5e1b9cc0
Suresh Katwal <katsurace@gmail.com>
2018-07-23 00:33:44 +0100

    search query is done:

---
+++ src/Controller/CommentAdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CommentAdminController extends Controller
+{
+    /**
+     * @Route("/admin/comment", name="comment_admin")
+     */
+    public function index(CommentRepository $repository, Request $request)
+    {
+        $q = $request->query->get('q');
+        $comments = $repository->findBySearchQuery($q);
+        return $this->render('comment_admin/index.html.twig', [
+            'comments' => $comments,
+        ]);
+    }
+}
